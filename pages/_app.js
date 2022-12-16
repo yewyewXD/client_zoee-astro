ce7ae327a8730c70e3cbb5926cda81acf71add83
yewyewXD <yewyew6933@gmail.com>
2022-12-16 13:28:12 +0800

    add source sans pro google font

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
+import { Source_Sans_Pro } from "@next/font/google";
+
+const ssp = Source_Sans_Pro({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <main className={ssp.className}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </main>
   );
 }
 
