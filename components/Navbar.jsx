b8cf69de7ebed57a846068555fc79675127fb7a9
yewyewXD <yewyew6933@gmail.com>
2023-01-07 15:21:24 +0800

    basic nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import navItems from "../json/navItems.json";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="container">test</div>
+    <nav className="fixed right-0 left-0 z-50">
+      <div className="container text-white flex items-center">
+        <div>
+          <Image
+            src="/images/logo-transparent.png"
+            alt=""
+            width={120}
+            height={120}
+          />
+        </div>
+        <div className="ml-auto mt-10">
+          {navItems.map((navItem) => (
+            <Link href={navItem.link} key={navItem.id}>
+              <span className="mx-4 text-xl">{navItem.text}</span>
+            </Link>
+          ))}
+        </div>
+      </div>
     </nav>
   );
 };
