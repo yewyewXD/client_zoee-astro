4c9a97a770126c48631e3e1708e5de9cca69a42c
yewyewXD <yewyew6933@gmail.com>
2023-02-01 15:38:19 +0800

    footer style

--- components/UI/Footer.jsx
+++ components/UI/Footer.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import BgOverlay from "./BgOverlay";
 
 const Footer = () => {
   return (
-    <footer className="py-4 px-6 text-center bg-red text-white">
-      Copyright {new Date().getFullYear()} @ easy.astrology.by.zoee
+    <footer
+      className="text-center text-white relative"
+      style={{
+        background: "url(/images/hero-bg.jpg) no-repeat center center / cover",
+      }}
+    >
+      <BgOverlay opacity={"0.4"} />
+      <div className="relative sm:py-8 py-4 px-6 font-semibold">
+        Copyright {new Date().getFullYear()} @ easy.astrology.by.zoee
+      </div>
     </footer>
   );
 };
