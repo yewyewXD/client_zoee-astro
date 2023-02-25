ec818560edbfade548702d5203d01901f1e63c58
yewyewXD <yewyew6933@gmail.com>
2023-02-25 12:54:16 +0800

    last fix

--- components/Home/Services.jsx
+++ components/Home/Services.jsx
@@ -101,8 +101,8 @@ const Services = () => {
                   service.alt ? "2xl:pr-40 md:pr-20" : "2xl:pl-40 md:pl-20"
                 } md:text-left text-center md:mt-0 mt-8`}
               >
-                <div className="2xl:text-4xl sm:text-3xl text-2xl font-bold relative flex flex-wrap sm:justify-start justify-center">
-                  <div className="sm:mr-2">{service.title}</div>
+                <div className="2xl:text-4xl sm:text-3xl text-2xl font-bold relative flex flex-wrap md:justify-start justify-center">
+                  <div className="mr-2">{service.title}</div>
                   <div>({service.price})</div>
                 </div>
 
